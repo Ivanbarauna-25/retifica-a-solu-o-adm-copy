0837db2d3b187d805e748591a1d687d929d0fabb
Ivan Barauna dos santos <ivanbarauna20@gmail.com>
2025-12-07 02:06:25 +0000

    File changes

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,4 +1,5 @@
-import * as React from "react"
+
+import React from "react"
 
 const Separator = React.forwardRef(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
   <div
--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 
 const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
