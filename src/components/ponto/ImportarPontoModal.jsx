b2e6fb2d4111b8e1aa245f3514b2d38c4b2d0ae0
Ivan Barauna <ivanbarauna18@gmail.com>
2026-01-23 02:54:06 +0000

    File changes

--- src/components/ponto/ImportarPontoModal.jsx
+++ src/components/ponto/ImportarPontoModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef, useMemo, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
